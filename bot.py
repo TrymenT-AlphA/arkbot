3962843b9f6cced2431c593e92f818fdd7e23a18
ChongKai <1302393176@qq.com>
2022-06-03 11:29:34 +0800

    init

--- bot.py
+++ bot.py
@@ -1,19 +1,42 @@
 # encoding:utf-8
-
 import nonebot
 from nonebot.adapters.onebot import V11Adapter as ONEBOT_V11ADAPTER
 
 # You can pass some keyword args config to init function
 nonebot.init()
-
+# get app
 app = nonebot.get_asgi()
-
+# get driver
 driver = nonebot.get_driver()
 driver.register_adapter(ONEBOT_V11ADAPTER)
 
 # Load plugins
 nonebot.load_builtin_plugins("echo")
 nonebot.load_from_toml("pyproject.toml")
+
+
+@driver.on_bot_connect
+async def onBotConnect():
+    bot = nonebot.get_bot()
+    await bot.call_api(
+        'send_group_msg',
+        group_id = '645350897',
+        message = '连接成功！高性能ですから!'
+    )
+
+
+@driver.on_bot_connect
+async def onBotConnect():
+    bot = nonebot.get_bot()
+    try:
+        await bot.call_api(
+            'send_group_msg',
+            group_id = '645350897',
+            message = '失联中...'
+        )
+    except:
+        print('失联中...')
+
 
 if __name__ == "__main__":
     nonebot.logger.warning(

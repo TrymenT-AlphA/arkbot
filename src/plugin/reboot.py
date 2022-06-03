15db8c5ddf77d224df3401e21a82233cfdf54072
ChongKai <1302393176@qq.com>
2022-06-03 13:59:01 +0800

    pass

--- src/plugin/reboot.py
+++ src/plugin/reboot.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-from nonebot import on_command
+from nonebot import on_command, get_bot
 from nonebot.rule import to_me
 from nonebot.permission import SUPERUSER
 from nonebot.matcher import Matcher
@@ -21,7 +21,12 @@ async def rebootHandler(matcher: Matcher) -> None:
     """
     重启
     """
-    await matcher.send('重启中...')
+    bot = get_bot()
+    await bot.call_api(
+        'send_group_msg',
+        group_id = '645350897',
+        message = '重启中...'
+    )
     sys.stdout.flush()
     program = sys.executable
     os.execl(program, program, *sys.argv)

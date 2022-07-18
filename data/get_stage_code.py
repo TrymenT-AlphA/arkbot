d650734e234ff2c78da31178813b13dcbf61dd25
ChongKai <1302393176@qq.com>
2022-07-19 03:21:53 +0800

    get_stage_code

---
+++ data/get_stage_code.py
@@ -0,0 +1,13 @@
+"""生成stage_code.json~
+"""
+import json
+
+
+if __name__ == '__main__':
+    res = {}
+    with open('../arksrc/gamedata/excel/stage_table.json', 'r', encoding='utf8') as  _:
+        info = json.load(_)['stages']
+    for key in info.keys():
+        res[key] = info[key]['code']
+    with open('stage_code.json', 'w', encoding='utf8') as  _:
+        json.dump(res,  _, ensure_ascii=False)

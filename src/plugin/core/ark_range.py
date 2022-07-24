22d118384c4c73e2466a586809a38fa593fe5c84
ChongKai <1302393176@qq.com>
2022-07-24 08:50:45 +0800

    v1

--- src/plugin/core/ark_range.py
+++ src/plugin/core/ark_range.py
@@ -4,8 +4,8 @@
 from typing import Optional
 from tqdm import tqdm
 from nonebot.log import logger
-from .data_base import Database
-from ..utils import json_to_obj, obj_to_json_str, json_str_to_obj
+from .data_base import MySQL
+from ..utils import json_to_obj
 
 
 class ArkRange:
@@ -19,26 +19,25 @@ class ArkRange:
         返回值:
             int: 更新的条目数
         """
-        _db = Database()
+        _db = MySQL()
         update_row = 0
-        # 1. 更新 range_table.json
+
+        # 更新 range_table.json
         logger.success('开始更新range_table')
         info = json_to_obj('arksrc/gamedata/excel/range_table.json')
         for _key, _val in tqdm(info.items()):
-            sql = """SELECT `id` FROM `range_table` WHERE `id`=%s"""
-            args = obj_to_json_str(_key)
-            _db.execute(sql, args)
-            if _db.fetchone() is not None:
+            if _db.select_one(
+                    table='range_table',
+                    keys=('id',),
+                    condition='`id`=%s',
+                    args=(_key,)
+            ):
                 continue
-            keys, values = zip(*_val.items())
-            sql = f"""INSERT INTO `range_table`
-                ({','.join([f"`{_k}`" for _k in keys])})
-                VALUES ({','.join(['%s'] * len(values))})
-                """
-            args = tuple(map(obj_to_json_str, values))
-            _db.execute(sql, args)
+            keys, vals = zip(*_val.items())
+            _db.insert('range_table', keys, vals)
             update_row += 1
         logger.success('range_table更新完毕!')
+
         return update_row
 
     def __init__(self, _id: str = None) -> None:
@@ -50,14 +49,15 @@ class ArkRange:
         返回值:
             str: 对应的html代码
         """
-        _db = Database()
-        sql = """SELECT `grids` FROM `range_table` WHERE id =%s"""
-        args = obj_to_json_str(self._id)
-        _db.execute(sql, args)
-        res = _db.fetchone()
-        if res is None:
-            return None
-        res = json_str_to_obj(res[0])
+        _db = MySQL()
+        res = _db.select_one(
+            table='range_table',
+            keys=('grids',),
+            condition='`id`=%s',
+            args=(self._id,)
+        )
+        if not res:
+            return
         coordinates = []
         min_row, min_col = 0, 0
         max_x, max_y = 0, 0
@@ -74,8 +74,8 @@ class ArkRange:
             coordinates[i][1] -= 26 * min_row
             max_y = max(max_y, coordinates[i][1])
         res = f"""
-            <svg xmlns="http://www.w3.org/2000/svg" 
-                 xmlns:xlink="http://www.w3.org/1999/xlink" 
+            <svg xmlns="http://www.w3.org/2000/svg"
+                 xmlns:xlink="http://www.w3.org/1999/xlink"
                  style="vertical-align:top;width:{max_x + 24}px;height:{max_y + 24}px">
             <defs>
                 <rect id="1" fill="#27a6f3" width="22" height="22"></rect>
